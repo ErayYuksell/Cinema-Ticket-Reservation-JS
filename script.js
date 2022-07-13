27793d9414d3b012ffa67f87287f2d14925568a3
Eray Yüksel <yukseleray203@gmail.com>
2022-07-13 22:03:57 +0300

    Bilet bilgileri Local storage kaydedildi

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container=document.querySelector('.container');
 const count=document.getElementById('count');
 const amount=document.getElementById('amount');
 const select=document.getElementById('movie');
-
+const seats=document.querySelectorAll('.seat:not(.reserved)')
 
 
 container.addEventListener('click',function(e){
@@ -20,10 +20,32 @@ select.addEventListener('change',function(e){
 
 
 function calculatetotal(){
-    let selectedseatcount=container.querySelectorAll('.seat.selected').length;
-    console.log(selectedseatcount);
-    let price=select.value;
-    count.innerText=selectedseatcount;
-    amount.innerText=selectedseatcount*price;
+   const selectedseats=container.querySelectorAll('.seat.selected');
+   let selectedseatcount=selectedseats.length;
+   const selectedseatsarr=[];
+   const seatsarr=[];
+   selectedseats.forEach(function(seat){
+   selectedseatsarr.push(seat);
+
+});
+    seats.forEach(function(seat){
+    seatsarr.push(seat);
     
+    });
+
+    let selectedseatindexs=selectedseatsarr.map(function(seat){
+    return seatsarr.indexOf(seat);
+
+    });
+    
+    count.innerText=selectedseatcount;
+    amount.innerText=selectedseatcount*select.value;
+
+    savetolocalstorage(selectedseatindexs);
+    
+}
+
+function savetolocalstorage(indexs){
+localStorage.setItem('selectedseats',JSON.stringify(indexs));
+localStorage.setItem('selectmovieindex',select.selectedIndex);
 }
